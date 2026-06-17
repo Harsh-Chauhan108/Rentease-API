690e3416ef7288d6ece7a465b7f4e211c4df5bc1
Harsh Chauhan <forskills705@gmail.com>
2026-06-17 19:54:05 +0530

    registration

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DB_URL ="sql+pymysql://root:9713@localhost:3306/rentease"
+DB_URL ="mysql+pymysql://root:9713@localhost/rentease"
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- auth.py
+++ auth.py
@@ -1,7 +1,12 @@
 
-from database import SessionLocal
+from database import SessionLocal,Base,engine
 from fastapi import Depends, HTTPException, status,FastAPI
 from hashing import verify_password, hash_password
+import schemas,models
+from sqlalchemy.orm import Session
+
+app=FastAPI()
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -9,3 +14,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.post("/register")
+
+def register(user: schemas.RegisterSchema ,
+             db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        return {'msg':'User already exists'}
+    hashed_password =hash_password(user.password)
+    new_user = models.User(name=user.name,email=user.email,password=hashed_password,role=user.role)
+    db.add(new_user)
+    db.commit()
+    return {'msg':'User registered successfully'}
