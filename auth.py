e69f2fe75febced82cf331dc03ea66df65c71982
Harsh Chauhan <forskills705@gmail.com>
2026-06-23 11:40:15 +0530

    booking apis

--- auth.py
+++ auth.py
@@ -193,3 +193,38 @@ current_user = Depends(get_current_user)
     return {
     "message": "Property Deleted"
     }
+
+@app.post("/booking")
+def create_booking(
+booking: schemas.BookingSchema,
+db: Session = Depends(get_db),
+current_user = Depends(get_current_user)
+):
+    if current_user.role != "tenant":
+        raise HTTPException(
+        403,
+        "Only tenants can book properties"
+        )
+    property = db.query(models.Property).filter(
+    models.Property.id == booking.property_id
+    ).first()
+    if not property:
+        raise HTTPException(404, "Property not found")
+    new_booking = models.Booking(
+    tenant_id=current_user.id,
+    property_id=booking.property_id
+    )
+    db.add(new_booking)
+    db.commit()
+    return {
+    "message": "Booking Request Sent"
+    }
+@app.get("/my-bookings")
+def my_bookings(
+db: Session = Depends(get_db),
+current_user = Depends(get_current_user)
+):
+    bookings = db.query(models.Booking).filter(
+    models.Booking.tenant_id == current_user.id
+    ).all()
+    return bookings
