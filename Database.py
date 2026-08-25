bfbb05600d2178993b537d68266fd3ed3f975371
Harsh Chauhan <forskills705@gmail.com>
2026-08-25 17:56:29 +0530

    Dockerize

--- Database.py
+++ Database.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+load_dotenv()
 DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
